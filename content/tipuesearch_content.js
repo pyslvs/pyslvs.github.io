9ed81fcadaf1fc56faf16eb66ac806057bb97126
mdecycu <mdecycuorg@gmail.com>
2023-12-02 19:54:21 +0800

    add old data link

--- content/tipuesearch_content.js
+++ content/tipuesearch_content.js
@@ -1,1 +1,1 @@
-var tipuesearch = {"pages": [{'title': 'About', 'text': 'cmsimde_site', 'tags': '', 'url': 'About.html'}]};
+var tipuesearch = {"pages": [{'title': 'About', 'text': 'Old data:  https://pyslvs.github.io/pyslvs.github.io_old  ', 'tags': '', 'url': 'About.html'}]};
